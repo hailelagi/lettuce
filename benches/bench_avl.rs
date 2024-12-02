cd2fc49b22a1c026fc676db4fd29eed07d760d1d
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2024-12-03 00:39:10 +0100

    wip
    Signed-off-by: Haile Lagi <52631736+hailelagi@users.noreply.github.com>

--- benches/bench_avl.rs
+++ benches/bench_avl.rs
@@ -1,28 +1,28 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use lettuce::weak_avl::WavlTree;
+// use criterion::{black_box, criterion_group, criterion_main, Criterion};
+// use lettuce::weak_avl::WavlTree;
 
-fn benchmark_insert(c: &mut Criterion) {
-    c.bench_function("wavl insert", |b| {
-        b.iter(|| {
-            let mut tree = WavlTree::new();
-            for i in 0..1000 {
-                tree.insert(black_box(i), i);
-            }
-        });
-    });
-}
+// fn benchmark_insert(c: &mut Criterion) {
+//     c.bench_function("wavl insert", |b| {
+//         b.iter(|| {
+//             let mut tree = WavlTree::new();
+//             for i in 0..1000 {
+//                 tree.insert(black_box(i), i);
+//             }
+//         });
+//     });
+// }
 
-fn benchmark_contains(c: &mut Criterion) {
-    c.bench_function("wavl find", |b| {
-        b.iter(|| {
-            let mut tree = WavlTree::new();
-            for i in 0..1000 {
-                tree.insert(i, i);
-            }
-            // tree.contains(black_box(&500));
-        });
-    });
-}
+// fn benchmark_contains(c: &mut Criterion) {
+//     c.bench_function("wavl find", |b| {
+//         b.iter(|| {
+//             let mut tree = WavlTree::new();
+//             for i in 0..1000 {
+//                 tree.insert(i, i);
+//             }
+//             // tree.contains(black_box(&500));
+//         });
+//     });
+// }
 
-criterion_group!(benches, benchmark_insert, benchmark_contains);
-criterion_main!(benches);
+// criterion_group!(benches, benchmark_insert, benchmark_contains);
+// criterion_main!(benches);
---
+++ src/art/mod.rs
@@ -0,0 +1,2 @@
+mod art;
+mod node;
