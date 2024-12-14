ab9d526561700b790aff6928c651a6c5e2d30ea7
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2024-12-15 00:02:39 +0100

    try pprof with flamegraph
    Signed-off-by: Haile Lagi <52631736+hailelagi@users.noreply.github.com>

--- benches/bench_avl.rs
+++ benches/bench_avl.rs
@@ -1,28 +1,32 @@
-// use criterion::{black_box, criterion_group, criterion_main, Criterion};
-// use lettuce::weak_avl::WavlTree;
+use std::process::Output;
 
-// fn benchmark_insert(c: &mut Criterion) {
-//     c.bench_function("wavl insert", |b| {
-//         b.iter(|| {
-//             let mut tree = WavlTree::new();
-//             for i in 0..1000 {
-//                 tree.insert(black_box(i), i);
-//             }
-//         });
-//     });
-// }
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use lettuce::avl::AVLTree;
+use pprof::validate;
 
-// fn benchmark_contains(c: &mut Criterion) {
-//     c.bench_function("wavl find", |b| {
-//         b.iter(|| {
-//             let mut tree = WavlTree::new();
-//             for i in 0..1000 {
-//                 tree.insert(i, i);
-//             }
-//             // tree.contains(black_box(&500));
-//         });
-//     });
-// }
+fn benchmark_insert(c: &mut Criterion) {
+    c.bench_function("wavl insert", |b| {
+        b.iter(|| {
+            let mut tree = AVLTree::new();
+            for i in 0..1000 {
+                tree.insert(black_box(i), i);
+            }
+        });
+    });
+}
 
-// criterion_group!(benches, benchmark_insert, benchmark_contains);
-// criterion_main!(benches);
+fn benchmark_contains(c: &mut Criterion) {
+    c.bench_function("wavl find", |b| {
+        b.iter(|| {
+            let mut tree = AVLTree::new();
+            for i in 0..1000 {
+                tree.insert(i, i);
+            }
+            // tree.contains(black_box(&500));
+        });
+    });
+}
+
+criterion_group!(benches, benchmark_insert, benchmark_contains);
+
+criterion_main!(benches);
